6450070e7ed556c0748eaf46e8696d72876417e0
Dzung Pean <hoangtudauphong@gmail.com>
2021-02-23 23:51:38 -0800

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use((req, res, next)=>{
+  console.log('First middleware');
+  next();
+})
+app.use((req,res,next)=>{
+  res.send('Hi from express');
+});
+
+module.exports = app;
+
