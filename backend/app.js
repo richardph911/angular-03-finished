17982acc5db9cd92b325fcdb066efbb73c34d80e
Dzung Pean <hoangtudauphong@gmail.com>
2021-02-24 23:48:23 -0800

    fetch post, add httpclient and addPost()

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
-app.use((req, res, next)=>{
-  console.log('First middleware');
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.json());
+app.use((req,res,next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST,PATCH, DELETE, OPTIONS");
   next();
-})
-app.use((req,res,next)=>{
-  res.send('Hi from express');
+});
+app.post("/api/posts",(req, res, next)=>{
+  const post = req.body;
+  res.status(201).json({
+    message: 'Post added successfully',
+
+  });
+});
+/*create path /api/posts and return a response in form of json*/
+app.get("/api/posts",(req,res,next) => {
+  const posts = [
+    {
+      id: 'fafss2233',
+      title: 'First post',
+      content: 'Content of the first post'
+    }
+  ];
+  res.status(200).json(
+    {
+      message: 'Post successfully',
+      posts: posts
+    }
+)
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const app = require('./backend/app');
 const port = process.env.PORT || 3000;
 app.set('port',port);
+//create server from backend app.js
 const server = http.createServer(app);
 
 server.listen(port);
